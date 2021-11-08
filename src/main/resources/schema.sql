a2f406313b077035ed466d504b8c5dccaaab24f0
hugog <hugog@LAPTOP-JD3LVJ0B>
2021-11-08 15:43:23 +0100

    hola

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 --Primero se deben borrar todas las tablas(de detalle a maestro) y lugo anyadirlas (de maestro a detalle)
---(en este caso en cada una de las aplicaciones (tkrun y descuento) se usa solo una tabla, por lo que no hace falta)
+--(en este caso en cada una de las aplicaciones (tkrun y descuento) se usa solo una tabla, por lo que no hace falta) 
 
 --Para giis.demo.tkrun:
 
