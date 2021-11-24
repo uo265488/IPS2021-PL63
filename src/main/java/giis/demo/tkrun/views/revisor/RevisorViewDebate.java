7d1990c28934155847550fceb5e1e201ea2e3cda
hugog <hugog@LAPTOP-JD3LVJ0B>
2021-11-24 12:21:58 +0100

    Se empieza la siguiente historia

---
+++ src/main/java/giis/demo/tkrun/views/revisor/RevisorViewDebate.java
@@ -0,0 +1,42 @@
+package giis.demo.tkrun.views.revisor;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class RevisorViewDebate extends JFrame {
+
+    private JPanel contentPane;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+	EventQueue.invokeLater(new Runnable() {
+	    public void run() {
+		try {
+		    RevisorViewDebate frame = new RevisorViewDebate();
+		    frame.setVisible(true);
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+	    }
+	});
+    }
+
+    /**
+     * Create the frame.
+     */
+    public RevisorViewDebate() {
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setBounds(100, 100, 600, 483);
+	contentPane = new JPanel();
+	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	setContentPane(contentPane);
+	contentPane.setLayout(null);
+    }
+
+}
